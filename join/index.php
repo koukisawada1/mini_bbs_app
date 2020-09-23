0b0f0dd5cd25c22118e4d1cda2217bfe8b815b2b
澤田こうき <63055099+koukisawada1@users.noreply.github.com>
2020-09-23 18:30:57 +0900

    ログインページエラーチェック

--- join/index.php
+++ join/index.php
@@ -1,3 +1,29 @@
+<!-- エラーの確認 -->
+<?php
+if (!empty($_POST)) {
+	if ($_POST['name'] === '') {
+		$error['name'] = 'blank';
+	}
+	if ($_POST['email'] === '') {
+		$error['email'] = 'blank';
+	}
+	// パスワードの文字数の確認 strlen指定された文字数の文字の長さを返してくれる
+	if (strlen($_POST['password']) < 4) {
+		$error['password'] = 'length';
+	}
+	if ($_POST['password'] === '') {
+		$error['password'] = 'blank';
+	}
+	// エラーがないときにcheck.phpにジャンプする
+	if (empty($error)) {
+		header('Location: check.php');
+		exit();
+	}
+	error_reporting(E_ALL & ~E_NOTICE);
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -20,14 +46,30 @@
 	<dl>
 		<dt>ニックネーム<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="name" size="35" maxlength="255" value="" />
+        	<input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['name'], ENT_QUOTES)); ?>" />
+			<!-- エラーメッセージ -->
+			<?php if ($error['name'] === 'blank'): ?>
+				<p class="error">※ニックネームを入力してください</p>
+			<?php endif; ?>
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+        	<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>" />
+			<?php if ($error['email'] === 'blank'): ?>
+				<p class="error">※メールアドレスを入力してください</p>
+			<?php endif; ?>
+
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
+			<?php if ($error['password'] === 'length'): ?>
+				<p class="error">※パスワードは４文字以上で入力してください</p>
+			<?php endif; ?>
+			<?php if ($error['password'] === 'blank'): ?>
+				<p class="error">※パスワードを入力してください</p>
+			<?php endif; ?>
+
+
         </dd>
 		<dt>写真など</dt>
 		<dd>
